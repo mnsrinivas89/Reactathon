297a1bcfcc38a6d423db6fb0becc769b63b4838e
mnsrinivas89 <nagendrasrinivas08@gmail.com>
2018-07-03 08:41:18 +0530

    Image changes done .

--- App/components/Details.js
+++ App/components/Details.js
@@ -12,15 +12,15 @@ class Details extends Component {
    return (
    	<div className="detailsContainer row">
    		<h1 className="fontSize_8 title">Hackathon title</h1>	
-	     <div className="container padd detailsItem card col-xs-8 padd"> 	       
-	       <div className="teamDetailsContainer row noMargin">
-	       	<img src={DefaultImage} width="200px" alt="Team Image" className="col-xs-2" />
+	     <div className="container padd detailsItem card col-xs-8 padd"> 
+	       <img src={DefaultImage} width="200px" alt="Team Image" className="col-xs-2" />		       
+	       <div className="teamDetailsContainer row noMargin">	       	
 	       	<h2 className="fontSize_7">Team Name</h2>
 	       	<div className="margin10 noSideMargin">Team Description</div>	
 	       	<div className="row col-xs-10">	
-	       		<div className="col-xs-6">Team 1</div>
-	       		<div className="col-xs-6">Team 2</div>
-	       		<div className="col-xs-6">Team 3</div>
+	       		<div className="col-xs-6 noPad">Team 1</div>
+	       		<div className="col-xs-6 noPad">Team 2</div>
+	       		<div className="col-xs-6 noPad">Team 3</div>
 	       	</div>	 
 	       </div>       
 	   		<div className="row noMargin">
